a683507fbd51821a084e9d6f834cfcb217f468c6
freekof <freekof8@gmail.com>
2026-05-11 23:11:41 +0800

    Revert "fix: return proxy application status"
    This reverts commit 122c97a41975ea47af81bd4486bbd6f9093e063f.

--- app/src/main/java/com/freekof/tvbrowser/WebViewProxyApplier.kt
+++ app/src/main/java/com/freekof/tvbrowser/WebViewProxyApplier.kt
@@ -26,7 +26,6 @@ object WebViewProxyApplier {
                 .addBypassRule("localhost")
                 .build()
             ProxyController.getInstance().setProxyOverride(proxyConfig, context.mainExecutor) { onApplied(true) }
-            true
         }.getOrElse {
             onApplied(false)
             false
@@ -40,7 +39,6 @@ object WebViewProxyApplier {
         }
         return runCatching {
             ProxyController.getInstance().clearProxyOverride(context.mainExecutor) { onApplied(true) }
-            true
         }.getOrElse {
             onApplied(false)
             false
